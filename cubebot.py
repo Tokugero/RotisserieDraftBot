0a744f973f75ab3615560b812a7658ff871c7356
Phil Almueti <palmueti@gmail.com>
2019-02-17 10:48:59 -0800

    Added p1p1 functionality v1

---
+++ cubebot.py
@@ -0,0 +1,28 @@
+from discord.ext import commands
+import asyncio
+import os
+import logging
+import sys
+from functions import cubebot
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
+logger = logging.getLogger('cubebot.main')
+
+#EnvironmentLoaders
+botToken = os.environ['CUBEBOT']
+
+#Initializing bot
+description = '''CubeBot Commands'''
+bot = commands.Bot(command_prefix='$', description=description)
+
+@bot.event
+async def on_ready():
+    logger.info('Logged in as')
+    logger.info(bot.user.name)
+    logger.info(bot.user.id)
+    logger.info('------')
+
+
+bot.load_extension("functions.cubebot")
+
+bot.run(botToken)
---
+++ functions/cubebot.py
@@ -0,0 +1,36 @@
+from functions import cubetutor
+import logging
+import discord
+import sys
+import asyncio
+import random
+import requests
+from discord.ext import commands
+
+#shahrazad token ed98810941cf53fa9806f9aa9299c2 - This is only available internally and only executes the shahrazad test pipeline
+logger = logging.getLogger('cubebot.cubebot')
+
+logger.info("cubebot cog loaded")
+
+class Application:
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command(pass_context=True)
+	async def p1p1(self, ctx):
+		"""Generate a P1P1 image given a cubetutor cube ID"""
+		command = ctx.message.content.split()
+		if len(command) > 1:
+			try:
+				cubeId = int(command[1])
+				logger.info(str(ctx.message.author) + " pulled a pack")
+				await ctx.channel.send("Looking up pack...")
+				cubeId = command[1]
+				packLoader = self.bot.loop.create_task(cubetutor.CubeTutorPackChrome(cubeId, ctx))
+			except:
+				await ctx.channel.send("Please give me a real ID at least. ")
+		if len(command) is 1:
+			await ctx.channel.send("I need an ID.")
+
+def setup(bot):
+	bot.add_cog(Application(bot))
